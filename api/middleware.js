54ba49292bac5ae1274a348d3ad07d76ee9fe649
unknown <adamcal@protonmail.com>
2021-03-23 15:56:08 +0100

    remove unused code

--- api/middleware.js
+++ api/middleware.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const axios = require('axios')
 const jwt = require("jsonwebtoken")
 const jwtSecret = process.env.JWT_SECRET
-const hcaptchaSecret = process.env.HCAPTCHA_SECRET
 
 const verifyToken = (req, res, next) => {
   let token = req.headers['authorization']
@@ -15,19 +14,6 @@ const verifyToken = (req, res, next) => {
     req._id = decoded.id
     next()
   })
-}
-
-const hcaptcha = async (req, res, next) => {
-  const data = `response=${req.body['h-captcha-response']}&secret=${hcaptchaSecret}`
-  const url = 'https://hcaptcha.com/siteverify'
-
-  const response = await axios.post(url, data, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
-        console.log(response['data'])
-      if (response['data']['success']) {
-          next()
-      } else {
-        return res.send({'message': 'hcaptcha failed'})
-      }
 }
 
 module.exports = {verifyToken, hcaptcha}
